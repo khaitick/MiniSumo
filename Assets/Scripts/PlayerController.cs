26b243985b43f174a8a341e0e58b6c358bddf0af
caden hl <62642278+khaitick@users.noreply.github.com>
2021-07-21 01:09:22 +0800

    Add powerup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 10f;
+    public float powerupStrength = 10f;
+
+    public float powerupCooldown = 5f;
+
+    public GameObject powerupIndicator;
     private Transform focalPoint;
     private Rigidbody rb;
+
+    private bool hasPowerup;
 
     void Start()
     {
@@ -18,5 +25,38 @@ public class PlayerController : MonoBehaviour
     {
         float verticalInput = Input.GetAxis("Vertical");
         rb.AddForce(focalPoint.forward * speed * verticalInput);
+
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            Destroy(other.gameObject);
+            StartCoroutine(Powerup());
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody rbEnemy = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 enemyDirection = collision.gameObject.transform.position - transform.position;
+
+            rbEnemy.AddForce(enemyDirection * powerupStrength, ForceMode.Impulse);
+
+            Debug.Log("enemy");
+        }
+    }
+
+    IEnumerator Powerup()
+    {
+        hasPowerup = true;
+        powerupIndicator.gameObject.SetActive(true);
+        yield return new WaitForSeconds(powerupCooldown);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
     }
 }
